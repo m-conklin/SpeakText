a8e0dea8f064070e304ea67959c0e429eb5dbd89
Martin Conklin <mjmc@uvic.ca>
2016-10-10 03:09:59 -0700

    Finalize UI colours and button flow.

--- speakText/speakText/ViewController.m
+++ speakText/speakText/ViewController.m
@@ -33,101 +33,15 @@
 
 @synthesize stopButton, playButton, recordButton, textView, saveButton;
 
-
-- (IBAction)recordButtonTapped:(UIButton *)sender {
-    if (player.playing) {
-        [player stop];
-    }
-    //    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
-
-    if (!recorder.recording) {
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
-
-            AVAudioSession *session = [AVAudioSession sharedInstance];
-            [session setActive:YES error:nil];
-            
-            //Begin recording
-            [recorder record];
-            
-            speechToTextBridge = [[SpeechToTextBridge alloc] init];
-            [speechToTextBridge startStreaming];
-        });
-        [recordButton setTitle:@"Pause" forState:UIControlStateNormal];
-
-    } else {
-        [recorder pause];
-        [recordButton setTitle:@"Record" forState:UIControlStateNormal];
-    }
-    [stopButton setEnabled:YES];
-    [playButton setEnabled:NO];
-}
-
-- (IBAction)stopButtonTapped:(UIButton *)sender {
-    [recorder stop];
-    [speechToTextBridge stopStreaming];
-    [recordButton setTitle:@"Record" forState:UIControlStateNormal];
-    [saveButton setEnabled:YES];
-//    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
-//        speechToTextBridge = [[SpeechToTextBridge alloc] init];
-//        [speechToTextBridge synthesize:outputFileURL];
-//    });
-    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
-    [audioSession setActive:NO error:nil];
-}
-
-- (IBAction)playButtonTapped:(UIButton *)sender {
-    if (!recorder.recording) {
-//        speechToTextBridge = [[SpeechToTextBridge alloc] init];
-//        [speechToTextBridge synthesize:outputFileURL];
-        player = [[AVAudioPlayer alloc] initWithContentsOfURL:recorder.url error:nil];
-        [player setDelegate:self];
-        [player play];
-    }
-}
-
-- (IBAction)saveButtonTapped:(UIButton *)sender {
-    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Save" message:@"Please enter a title" preferredStyle:UIAlertControllerStyleAlert];
-    
-    [alertController addTextFieldWithConfigurationHandler:^(UITextField *textField) {
-        textField.placeholder = @"Title";
-        textField.textColor = [UIColor grayColor];
-        textField.clearButtonMode = UITextFieldViewModeWhileEditing;
-        textField.borderStyle = UITextBorderStyleRoundedRect;
-    }];
-    
-    [alertController addAction:[UIAlertAction actionWithTitle:@"Save" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-        NSString *title = alertController.textFields[0].text;
-        [self saveRecording:title];
-    }]];
-    
-    [self presentViewController:alertController animated:YES completion:nil];
-}
-
--(void)saveRecording:(NSString *)title {
-    NSManagedObjectContext *managedContext = self.managedObjectContext;
-    NSManagedObject *newRecording = [NSEntityDescription insertNewObjectForEntityForName:@"Recording" inManagedObjectContext:managedContext];
-    [newRecording setValue:title forKey:@"title"];
-    [newRecording setValue:filename forKey:@"file"];
-    [newRecording setValue:textView.text forKey:@"transcript"];
-    int date = (int)[[NSDate date] timeIntervalSince1970];
-    NSNumber *dateRecorded = [NSNumber numberWithInteger:date];
-    [newRecording setValue:dateRecorded forKey:@"dateRecorded"];
-    
-    NSError *error = nil;
-    
-    if (![managedContext save:&error]) {
-        NSLog(@"Error saving to core data: %@ %@", error, [error localizedDescription]);
-    }
-}
-
-
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     //Initialize button states
     [stopButton setEnabled:NO];
     [playButton setEnabled:NO];
     [saveButton setEnabled:NO];
+    saveButton.hidden = YES;
+    playButton.hidden = YES;
+    self.textView.text = @"";
     
     self.navigationController.navigationBar.barTintColor = [UIColor colorWithRed:0.71 green:0.73 blue:0.76 alpha:1.0];
     
@@ -158,6 +72,137 @@
     recorder.meteringEnabled = YES;
     [recorder prepareToRecord];
 }
+
+
+
+- (IBAction)recordButtonTapped:(UIButton *)sender {
+    if (player.playing) {
+        [player stop];
+    }
+    if (!recorder.recording) {
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+
+            AVAudioSession *session = [AVAudioSession sharedInstance];
+            [session setActive:YES error:nil];
+            
+            //Begin recording
+            [recorder record];
+            
+            speechToTextBridge = [[SpeechToTextBridge alloc] init];
+            [speechToTextBridge startStreaming];
+        });
+        [recordButton setTitle:@"Pause" forState:UIControlStateNormal];
+
+    } else {
+        [recorder pause];
+        [recordButton setTitle:@"Record" forState:UIControlStateNormal];
+    }
+    [stopButton setEnabled:YES];
+    [playButton setEnabled:NO];
+}
+
+- (IBAction)stopButtonTapped:(UIButton *)sender {
+    [recorder stop];
+    [speechToTextBridge stopStreaming];
+    [recordButton setTitle:@"Record" forState:UIControlStateNormal];
+    [saveButton setEnabled:YES];
+    saveButton.hidden = NO;
+    playButton.hidden = NO;
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    [audioSession setActive:NO error:nil];
+}
+
+- (IBAction)playButtonTapped:(UIButton *)sender {
+    if (!recorder.recording) {
+        player = [[AVAudioPlayer alloc] initWithContentsOfURL:recorder.url error:nil];
+        [player setDelegate:self];
+        [player play];
+    }
+}
+
+- (IBAction)saveButtonTapped:(UIButton *)sender {
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Save" message:@"Please enter a title" preferredStyle:UIAlertControllerStyleAlert];
+    
+    
+    [alertController addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+        textField.placeholder = @"Title";
+        textField.textColor = [UIColor grayColor];
+        textField.clearButtonMode = UITextFieldViewModeWhileEditing;
+//        textField.borderStyle = UITextBorderStyleRoundedRect;
+    }];
+    
+    [alertController addAction:[UIAlertAction actionWithTitle:@"Save" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        NSString *title = alertController.textFields[0].text;
+        [self saveRecording:title];
+    }]];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil]];
+    alertController.view.tintColor = [UIColor darkGrayColor];
+    alertController.view.backgroundColor = [UIColor colorWithRed:0x2B green:0xAE blue:0xFF alpha:1.0];
+    
+//    alertController.v
+    [self presentViewController:alertController animated:YES completion:nil];
+}
+
+-(void)saveRecording:(NSString *)title {
+    NSManagedObjectContext *managedContext = self.managedObjectContext;
+    NSManagedObject *newRecording = [NSEntityDescription insertNewObjectForEntityForName:@"Recording" inManagedObjectContext:managedContext];
+    [newRecording setValue:title forKey:@"title"];
+    [newRecording setValue:filename forKey:@"file"];
+    [newRecording setValue:textView.text forKey:@"transcript"];
+    int date = (int)[[NSDate date] timeIntervalSince1970];
+    NSNumber *dateRecorded = [NSNumber numberWithInteger:date];
+    [newRecording setValue:dateRecorded forKey:@"dateRecorded"];
+    
+    NSError *error = nil;
+    
+    if (![managedContext save:&error]) {
+        NSLog(@"Error saving to core data: %@ %@", error, [error localizedDescription]);
+    } else {
+        [self viewDidLoad];
+    }
+}
+
+
+
+//- (void)viewDidLoad {
+//    [super viewDidLoad];
+//    //Initialize button states
+//    [stopButton setEnabled:NO];
+//    [playButton setEnabled:NO];
+//    [saveButton setEnabled:NO];
+//    saveButton.hidden = YES;
+//    playButton.hidden = YES;
+//    self.textView.text = @"";
+//    
+//    self.navigationController.navigationBar.barTintColor = [UIColor colorWithRed:0.71 green:0.73 blue:0.76 alpha:1.0];
+//    
+//    //Add listner
+//    NSNotificationCenter *ns = [NSNotificationCenter defaultCenter];
+//    [ns addObserver:self selector:@selector(updateTextView:) name:@"TextReturnedFromWatson" object:nil];
+//    
+//    //Audio Path
+//    filename = [NSString stringWithFormat:@"%d%@", (int) [[NSDate date] timeIntervalSince1970], @".wav"];
+//    
+//    NSArray *pathComponents = [NSArray arrayWithObjects:[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject],filename, nil];
+//    outputFileURL = [NSURL fileURLWithPathComponents:pathComponents];
+//    
+//    //Audio session
+//    AVAudioSession *session = [AVAudioSession sharedInstance];
+//    [session setCategory:AVAudioSessionCategoryPlayAndRecord error:nil];
+//    
+//    //Define recorder setting
+//    NSMutableDictionary *recordSetting = [[NSMutableDictionary alloc] init];
+//    
+//    [recordSetting setValue:[NSNumber numberWithInt:kAudioFormatLinearPCM] forKey:AVFormatIDKey];
+//    [recordSetting setValue:[NSNumber numberWithFloat:44100.0] forKey:AVSampleRateKey];
+//    [recordSetting setValue:[NSNumber numberWithInt:2] forKey:AVNumberOfChannelsKey];
+//    
+//    //Initialize recorder
+//    recorder = [[AVAudioRecorder alloc] initWithURL:outputFileURL settings:recordSetting error:NULL];
+//    recorder.delegate = self;
+//    recorder.meteringEnabled = YES;
+//    [recorder prepareToRecord];
+//}
 
 
 
