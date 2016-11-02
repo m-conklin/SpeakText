0394860e105b942adbe001cc2900236bf1228e87
Martin Conklin <mjmc@uvic.ca>
2016-11-01 18:39:59 -0700

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# objectiveC
+This is a demonstration app for iOS programming in Objective C while integrating the IBM Watson AI API using a Swift wrapper.
--- speakText/speakText/WatsonBridge.swift
+++ speakText/speakText/WatsonBridge.swift
@@ -11,7 +11,7 @@ import SpeechToTextV1
 import AVFoundation
 
 class SpeechToTextBridge: NSObject {
-    let speechToText = SpeechToText(username: "a", password: "b")
+    let speechToText = SpeechToText(username: "77b3137d-673a-4625-9ed9-e9be0b404902", password: "e8Y2hqO4lZla")
     
     var text:String?
     func startStreaming() {
